e0de97f1d969f7a7ed0991c6886b0def49182c03
evgeny <ekarpovsky@gmail.com>
2024-04-21 12:41:14 +0000

    Implement jwt-sql ext with new auth set

--- extensions/ses-sql/authentication.ts
+++ extensions/ses-sql/authentication.ts
@@ -1,14 +1,18 @@
-import { AuthMngrOPtions, setupAuthManager } from '../../packages/base-user-mngr/src';
-import { initStrategies, StrategyOptions } from '../../packages/auth-strategies/src';
 import { AuthConfig, SessionConfig, setupAuthMiddleware } from '../../packages/auth-session/src';
 import { BaseUser, dBApi, Token } from '../../packages/base-user-sql/src';
+import { cryptUtils, CryptUtilsOptions } from '../../packages/auth-utils/src';
+import { initVerify, VerifyOptions } from '../../packages/auth-verify-service/src';
+import { initStrategy as InitLocal, StrategyOptions } from '../../packages/auth-strategy-local/src';
+import { AuthMngrOptions, initAuthMngr } from '../../packages/auth-mngr/src';
+import { UserMngrOPtions, setupUserManager } from '../../packages/auth-user-mngr/src';
 
 // Reexport
 export { BaseUser, Token };
 
 // Setup Auth with session and Sql Db
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const setupAuthentication = ({ app, config, db, User }) => {
+export const setupAuthentication = (authOptions: any) => {
+  const { app, config, db, router, passport, User } = authOptions;
   console.log(db.name);
   // Wrap up the User and the Token
   const user = dBApi(db(User ? User : BaseUser));
@@ -16,37 +20,40 @@ export const setupAuthentication = ({ app, config, db, User }) => {
 
   // Setup the strategy and the user manager with the user
   // Strategy
-  const strategyOptions: StrategyOptions = {
+  const cryptUtilsOptions: CryptUtilsOptions = {
+    salt: Number(config.salt),
+  };
+
+  const utils = cryptUtils(cryptUtilsOptions);
+
+  const verifyOptions: VerifyOptions = {
     dBApi: user,
+    utils: utils,
   };
 
-  const strategy = initStrategies(strategyOptions);
+  const verifyUser = initVerify(verifyOptions);
 
-  // User manager
-  const authMngrOPtions: AuthMngrOPtions = {
-    User: user,
-    strategy: strategy,
-    session: true,
-    Token: token,
-    emailer: config.emailer,
+  const strategyOptions: StrategyOptions = {
+    verify: verifyUser,
+    loginFieldName: config.loginFieldName,
   };
-  const authRouter = setupAuthManager(authMngrOPtions);
+
+  const local = InitLocal(strategyOptions);
 
   // Session
   const sesConfig: SessionConfig = {
-    name: config.sessionName,
-    secret: config.sessionSecret,
-    saveUninitialized: config.sessionSaveUninitialized,
+    name: config.session.name,
+    secret: config.session.secret,
+    saveUninitialized: config.session.saveUninitialized,
     cookie: {
-      secure: config.cookieSecure,
-      sameSite: config.cookieSameSite,
-      httpOnly: config.cookieHttpOnly,
-      maxAge: config.cookieMaxAge,
+      secure: config.session.cookie.secure,
+      sameSite: config.session.cookie.sameSite,
+      httpOnly: config.session.cookie.httpOnly,
+      maxAge: config.session.cookie.maxAge,
     },
-    resave: config.sessionResave,
+    resave: config.session.sessionResave,
   };
 
-  // Auth middleware setup
   const authConfig: AuthConfig = {
     app: app,
     User: user,
@@ -54,5 +61,25 @@ export const setupAuthentication = ({ app, config, db, User }) => {
   };
   const authMiddleware = setupAuthMiddleware(authConfig);
 
-  return { authRouter, authMiddleware };
+  // Auth manager
+  const authMngrOptions: AuthMngrOptions = {
+    router: router,
+    passport: passport,
+    session: true,
+    encode: null,
+    strategies: [local],
+  };
+  const authRouter = initAuthMngr(authMngrOptions);
+
+  // User manager
+  const userMngrOPtions: UserMngrOPtions = {
+    User: user,
+    Token: token,
+    utils: utils,
+    session: true,
+    emailer: config.emailer,
+  };
+  const userMngrRouter = setupUserManager(userMngrOPtions);
+
+  return { authRouter, userMngrRouter, authMiddleware };
 };
