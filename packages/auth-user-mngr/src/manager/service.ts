661e247d670f06d3574809be03cb676e4c38cd7c
evgeny <ekarpovsky@gmail.com>
2024-05-03 14:53:00 +0000

    Remove unused service from auth-user-mngr

--- packages/auth-user-mngr/src/manager/service.ts
+++ packages/auth-user-mngr/src/manager/service.ts
@@ -5,33 +5,6 @@ import { UserMngrOPtions } from './types';
 export const setupUserMngrService = (options: UserMngrOPtions) => {
   const { User, Token, utils } = options;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const register = async (loginFieldName: string, newUser: any) => {
-    try {
-      if (!newUser[loginFieldName]) throw new Error('missing credentials');
-      if (loginFieldName === 'email') {
-        newUser[loginFieldName].toLowerCase();
-      };
-      const user = await User.findOne({ [loginFieldName]: newUser[loginFieldName] });
-      if (user) {
-        throw new Error('User already exist');
-      } else {
-        // const newUser = req.body;
-        newUser.password = await utils.hash(newUser.password);
-        newUser.createdAt = new Date();
-        try {
-          const user = await User.save(newUser);
-          return user;
-        } catch (e) {
-          throw(e);
-        }
-      }
-    } catch (e) {
-      throw(e);
-    }
-  };
-
-
   const changePassword = async (
     email: string,
     password: string,
@@ -126,7 +99,6 @@ export const setupUserMngrService = (options: UserMngrOPtions) => {
   };
 
   return {
-    register,
     changePassword,
     resetPasswordRequest,
     resetPassword,
